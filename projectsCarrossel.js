74188521589436edf82f045275e70c3e4029c071
CaioSSilva <caiosouzasilva13650@gmail.com>
2022-07-22 09:04:30 -0300

    Button Final

--- projectsCarrossel.js
+++ projectsCarrossel.js
@@ -1,12 +1,13 @@
 const carrousel = document.querySelector('.carrossel')
 const project = document.querySelectorAll('.projeto')
 const right = document.getElementById('right-btn')
+const left = document.getElementById('left-btn')
 const Container = document.getElementById('projetos')
 
 let Index = 0;
 
 right.addEventListener('click',()=>{
-    if(Index < ((project.length -1) * 290)){
+    if(Index < 1980){
         x = Math.round(carrousel.scrollWidth / project.length)
         Index = Index + x;
         carrousel.style.marginLeft = "-" + Index + "px";
@@ -16,6 +17,15 @@ right.addEventListener('click',()=>{
         Index = 0
     }
 })
+
+left.addEventListener('click',()=>{
+    if(Index != 0){
+        x = Math.round(carrousel.scrollWidth / project.length)
+        Index = Index - x
+        carrousel.style.marginLeft = "-" + Index + "px";
+    }
+})
+
 window.addEventListener("resize",()=>{
     if(carrousel.scrollWidth < Container.offsetWidth){
         right.style.display = "none";
